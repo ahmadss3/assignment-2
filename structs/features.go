c506702592827f64287b1debb11d87104f318487
Ahmad Suleiman Al-Jaabo <ahmadsa@stud.ntnu.no>
2025-04-01 01:32:29 +0200

    Added the file structs\features.go

---
+++ structs/features.go
@@ -0,0 +1,15 @@
+// File: assignment-2/structs/features.go
+package structs
+
+// Features defines which fields should be included in the dashboard.
+type Features struct {
+	Temperature      bool     `json:"temperature"`      // Temperature should be fetched.
+	Precipitation    bool     `json:"precipitation"`    // Precipitation should be fetched.
+	Capital          bool     `json:"capital"`          // The name of the capital city should be included.
+	Coordinates      bool     `json:"coordinates"`      // The latitude and longitude of the country should be included.
+	Population       bool     `json:"population"`       // The total population should be included.
+	Area             bool     `json:"area"`             // the total area should be included.
+	TargetCurrencies []string `json:"targetCurrencies"` // Is a list of currency codes for which the user
+	// wants to see exchange rates relative to the country's base currency.
+
+}
